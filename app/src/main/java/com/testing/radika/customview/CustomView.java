b93d23fc35836c87ab4283f0a2d23d1f43e10f73
EddLeon <edalx92@gmail.com>
2016-06-09 08:17:38 -0500

    first commit

---
+++ app/src/main/java/com/testing/radika/customview/CustomView.java
@@ -0,0 +1,35 @@
+package com.testing.radika.customview;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by greenapsis on 9/06/16.
+ */
+public class CustomView extends View {
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
